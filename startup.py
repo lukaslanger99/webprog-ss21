441795e9e243fa29694cba97f632b9e01b908560
Lukas Langer <lukas.langer99@gmx.de>
2021-05-19 19:48:41 +0200

    commts for script

--- startup.py
+++ startup.py
@@ -1,16 +1,20 @@
 import os
 import json
 
+# read files from dir
 songs = os.listdir('./files')
 
+# create jsonData object
 jsonData = {}
 jsonData['songs'] = []
 
+# write fileurls into json
 for song in songs:
     if song != '.gitignore' and song != '.gitkeep':
         jsonData['songs'].append({
             'songurl': song
         })
 
+# save json file
 with open('./json/songurls.json', 'w') as outfile:
     json.dump(jsonData, outfile)
